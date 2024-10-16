32c5fbd116c84b56bf16b796834c578c0c9073bc
Andy <Andydeuter2021@gmail.com>
2024-10-16 09:56:53 -0600

    se declaro grafo

--- practica.py
+++ practica.py
@@ -1,6 +1,6 @@
 class Grafo:
     def __init__(self) -> None:
-        self.grafo = ()
+        self.grafo = {}
 
     def agregar_nodo(self, nodo):
         if nodo  not in self.grafo:
@@ -15,13 +15,13 @@ class Grafo:
         for nodo in self.grafo:
             print(f"{nodo} : {self.grafo[nodo]}")
 
-    mi_grafo = Grafo()
-    mi_grafo.agregar_nodo('A')
-    mi_grafo.agregar_nodo('B')
-    mi_grafo.agregar_nodo('C')
-    mi_grafo.agregar_arista('A', 'B')
-    mi_grafo.agregar_arista('A', 'c')
+mi_grafo = Grafo()
+mi_grafo.agregar_nodo('A')
+mi_grafo.agregar_nodo('B')
+mi_grafo.agregar_nodo('C')
+mi_grafo.agregar_arista('A', 'B')
+mi_grafo.agregar_arista('A', 'c')
 
-    mi_grafo.mostrar_grafo()
+mi_grafo.mostrar_grafo()
 
-    print(mi_grafo.grafo)
+print(mi_grafo.grafo)
